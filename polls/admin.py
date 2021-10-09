5d459745e6838ace472f4085097745484332a9ee
Mathew Bushuru <bushuruwafulamathew@gmail.com>
2021-10-09 02:13:10 -0700

    create public interface by adding views and templates

--- polls/admin.py
+++ polls/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 #superuser: username-admin, pw=polladmin2021
 
 # Register your models here.
-from .models import Question
+from .models import Question,Choice
 
 admin.site.register(Question)
+admin.site.register(Choice)
+
